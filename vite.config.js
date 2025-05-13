9519672ff9b40f07e4d7ac1e3374f9a27ff0cecd
lancevd <olamosob@gmail.com>
2025-05-13 15:40:02 +0100

    config for deployment trying

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
 })
