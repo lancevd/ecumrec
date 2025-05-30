9d1713fe66a81582f5712782e14c79c1a83f4989
lancevd <olamosob@gmail.com>
2025-05-30 15:54:44 +0100

    set baseurl in env

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5670/api",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
