6b11e93b96cdcb3549dbab59cc66928458c93730
lancevd <olamosob@gmail.com>
2025-06-18 04:21:14 +0100

    Student directory live

--- src/pages/counselor/StudentDirectory.jsx
+++ src/pages/counselor/StudentDirectory.jsx
@@ -1,7 +1,29 @@
-import { Link } from 'react-router-dom';
-import { students } from '../../utils/data';
+import { Link } from "react-router-dom";
+import { useAssessment } from "../../api/counselors/useAssessments";
+import { useEffect, useState } from "react";
+import Spinner from "../../components/Spinner";
 
 export default function StudentDirectory() {
+  const { getMyAssessments, loading, error } = useAssessment();
+  const [students, setStudents] = useState(null);
+  const [totalPages, setTotalPages] = useState(1);
+  useEffect(() => {
+    fetchAssessments();
+  }, []);
+  const fetchAssessments = async () => {
+    try {
+      const response = await getMyAssessments("");
+      setStudents(response.data);
+      setTotalPages(response.pagination.totalPages || 1);
+    } catch (err) {
+      console.error("Error fetching assessments:", err);
+      setStudents();
+      setTotalPages(1);
+    }
+  };
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <div>
       <h1 className="text-2xl font-bold mb-6">Student Directory</h1>
@@ -12,13 +34,20 @@ export default function StudentDirectory() {
       />
       <div className="bg-white rounded-lg shadow divide-y">
         {students.map((student) => (
-          <div key={student.id} className="flex items-center justify-between p-4 hover:bg-gray-50">
+          <div
+            key={student._id}
+            className="flex items-center justify-between p-4 hover:bg-gray-50"
+          >
             <div>
-              <div className="font-semibold">{student.name}</div>
-              <div className="text-sm text-gray-500">Grade {student.grade} &bull; {student.email}</div>
+              <div className="font-semibold">
+                {student.studentId.firstName} {student.studentId.lastName}
+              </div>
+              <div className="text-sm text-gray-500">
+                {/* {student.studentId.admissionNumber} */}
+              </div>
             </div>
             <Link
-              to={`/counselor/students/${student.id}`}
+              to={`/counselor/students/${student.studentId._id}`}
               className="btn-primary font-medium hover:underline"
             >
               View Profile
@@ -28,4 +57,4 @@ export default function StudentDirectory() {
       </div>
     </div>
   );
-} 
+}
